e8777f749489bdaa901dd2d8ffca92a02e5f91f3
RPi2User <gm.aier1905@gmail.com>
2023-11-18 12:34:48 +0100

    Fixed Temp-File

--- nexaio_mail.sh
+++ nexaio_mail.sh
@@ -19,45 +19,45 @@ function Main(){
 }
 
 function ClearTemp(){
-	echo "$(hostname) Systemzusammenfassung:" > mail.tmp
-	echo "Beginn: $(date +%Y.%m.%d_%H:%M:%S)" >> mail.tmp
-	echo "Uptime: $(uptime)" >> mail.tmp
-	echo "" >> mail.tmp
+	echo "$(hostname) Systemzusammenfassung:" > /tmp/mail.tmp
+	echo "Beginn: $(date +%Y.%m.%d_%H:%M:%S)" >> /tmp/mail.tmp
+	echo "Uptime: $(uptime)" >> /tmp/mail.tmp
+	echo "" >> /tmp/mail.tmp
 	Seperator
-	echo "" >> mail.tmp
+	echo "" >> /tmp/mail.tmp
 }
 
 function Seperator(){ #OK!
-    echo "==============================================" >> mail.tmp
+    echo "==============================================" >> /tmp/mail.tmp
 }
 function ThinSeparator(){ #OK!
-    echo "----------------------------------------------" >> mail.tmp
+    echo "----------------------------------------------" >> /tmp/mail.tmp
 }
 
 function BackupNC(){
 	local exit_code=$?
 	backupFile="NC_$(date +%Y%m%d_%H%M%S).tar"
-	echo "Nextcloud wird gesichert..." >> mail.tmp
-	echo "" >> mail.tmp
-	echo "   Datei: $backupFile" >> mail.tmp
-	echo "   Dateisystem bereitstellen..." >> mail.tmp
-	mount -t ext4 /dev/disk/by-partuuid/18cbee9c-03 /mnt/backup >> mail.tmp
-	echo "   Entferne alte Backups" >> mail.tmp
-	rm $(ls -rt /mnt/backup/*.tar | head -n 1 ) &>> mail.tmp
-	echo "   Speicherplatz:" >> mail.tmp
-	df -h | grep --color=never /mnt/backup >> mail.tmp
-	echo "   Nextcloud stoppen..." >> mail.tmp
- #	nextcloud.occ maintenance:mode --on >> mail.tmp
+	echo "Nextcloud wird gesichert..." >> /tmp/mail.tmp
+	echo "" >> /tmp/mail.tmp
+	echo "   Datei: $backupFile" >> /tmp/mail.tmp
+	echo "   Dateisystem bereitstellen..." >> /tmp/mail.tmp
+	mount -t ext4 /dev/disk/by-partuuid/18cbee9c-03 /mnt/backup >> /tmp/mail.tmp
+	echo "   Entferne alte Backups" >> /tmp/mail.tmp
+	rm $(ls -rt /mnt/backup/*.tar | head -n 1 ) &>> /tmp/mail.tmp
+	echo "   Speicherplatz:" >> /tmp/mail.tmp
+	df -h | grep --color=never /mnt/backup >> /tmp/mail.tmp
+	echo "   Nextcloud stoppen..." >> /tmp/mail.tmp
+ #	nextcloud.occ maintenance:mode --on >> /tmp/mail.tmp
 	ThinSeparator
 	tar cf /mnt/backup/$backupFile /var/snap/nextcloud/common/nextcloud/data 2>/dev/null
 	if [ "$exit_code" = "" ]
 	then
-		echo "Fehlschlag Exitcode $exit_code" >> mail.tmp
+		echo "Fehlschlag Exitcode $exit_code" >> /tmp/mail.tmp
 	fi
-	echo "Ende: $(date +%Y.%m.%d_%H:%M:%S)" >> mail.tmp
+	echo "Ende: $(date +%Y.%m.%d_%H:%M:%S)" >> /tmp/mail.tmp
 	umount /mnt/backup
-	echo "Gebe Nextcloud wieder frei" >> mail.tmp
- #	nextcloud.occ maintenance:mode --off >> mail.tmp
+	echo "Gebe Nextcloud wieder frei" >> /tmp/mail.tmp
+ #	nextcloud.occ maintenance:mode --off >> /tmp/mail.tmp
 	Seperator
 }
 
@@ -65,36 +65,36 @@ function BackupNCAIO(){
     # NC-AIO uses BorgBackup as a AIO Backup-Solution
     # https://github.com/nextcloud/all-in-one#how-to-stopstartupdate-containers-or-trigger-the-daily-backup-from-a-script-externally
     # Describes Scripting of the Backup-Solution
-    echo "Starting NC-AIO Backup:" >> mail.tmp
-    docker exec -i --env DAILY_BACKUP=1 --env START_CONTAINERS=1 nextcloud-aio-mastercontainer /daily-backup.sh >> mail.tmp
-    echo "" >> mail.tmp
-    echo "End: $(uptime)" >> mail.tmp
-    echo "" >> mail.tmp
+    echo "Starting NC-AIO Backup:" >> /tmp/mail.tmp
+    docker exec -i --env DAILY_BACKUP=1 --env START_CONTAINERS=1 nextcloud-aio-mastercontainer /daily-backup.sh >> /tmp/mail.tmp
+    echo "" >> /tmp/mail.tmp
+    echo "End: $(uptime)" >> /tmp/mail.tmp
+    echo "" >> /tmp/mail.tmp
     Seperator
-    echo "" >> mail.tmp
+    echo "" >> /tmp/mail.tmp
 }
 
 function UpdateNC(){
     # Update NC-AIO
-    echo "Updating NC-AIO" >> mail.tmp
-    docker exec -i --env AUTOMATIC_UPDATES=1  nextcloud-aio-mastercontainer /daily-backup.sh >> mail.tmp
-    echo "" >> mail.tmp
-    echo "End: $(uptime)" >> mail.tmp
-    echo "" >> mail.tmp
+    echo "Updating NC-AIO" >> /tmp/mail.tmp
+    docker exec -i --env AUTOMATIC_UPDATES=1  nextcloud-aio-mastercontainer /daily-backup.sh >> /tmp/mail.tmp
+    echo "" >> /tmp/mail.tmp
+    echo "End: $(uptime)" >> /tmp/mail.tmp
+    echo "" >> /tmp/mail.tmp
     
 }
 
 function CheckUpdates(){
-	echo "Systemaktualisierungen:" >> mail.tmp
+	echo "Systemaktualisierungen:" >> /tmp/mail.tmp
 	apt update &>/dev/null
-	echo "APT:" >> mail.tmp
-	apt list --upgradeable -qq 1> mail.tmp 2>/dev/null
-	ThinSeparator >> mail.tmp
+	echo "APT:" >> /tmp/mail.tmp
+	apt list --upgradeable -qq 2>/dev/null 1> /tmp/mail.tmp 
+	ThinSeparator >> /tmp/mail.tmp
     UpdateNC
-	Seperator >> mail.tmp
+	Seperator >> /tmp/mail.tmp
 }
 function Send(){
-	cat mail.tmp | mail -s "Systemübersicht Raspberry Pi vom $(date +%d.%m.%Y)" florian@hatzfeld.biz
-	rm mail.tmp
+	cat /tmp/mail.tmp | mail -s "Systemübersicht Raspberry Pi vom $(date +%d.%m.%Y)" florian@hatzfeld.biz
+	rm /tmp/mail.tmp
 }
 Main
